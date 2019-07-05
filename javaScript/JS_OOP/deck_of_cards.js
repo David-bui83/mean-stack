e207a1c223c5596478acb90d9e47759be7a273c4
Mona Lisa <david.t.bui83@gmail.com>
2019-07-05 14:17:24 -0700

    landing page

--- javaScript/JS_OOP/deck_of_cards.js
+++ javaScript/JS_OOP/deck_of_cards.js
@@ -7,7 +7,6 @@ class Card{
   show(){
     return `${this.strValue} of ${this.suit}`;
   }
-
 }
 class Deck{
   constructor(){
---
+++ node/landing_page/app.js
@@ -0,0 +1,30 @@
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(function(req,res){
+  console.log('client request URL:', req.url);
+  if(req.url ==='/'){
+    fs.readFile('views/index.html','utf8',function(errors,contents){
+      res.writeHead(200,{'Content-Type':'text/html'});
+      res.write(contents);
+      res.end();
+    })
+  }else if(req.url === '/ninjas'){
+    fs.readFile('views/ninjas.html','utf8', function(errors,contents){
+      res.writeHead(200, {'Content-type': 'text/html'});
+      res.write(contents);
+      res.end();
+    })
+  }else if(req.url === '/dojos/new'){
+    fs.readFile('views/dojos.html', 'utf8', function(errors,contents){
+      res.writeHead(200, {'Content-type': 'text/html'});
+      res.write(contents);
+      res.end();
+    })
+  }else{
+    res.end('File not found!!!');
+  }
+})
+
+server.listen(6789);
+console.log('Running in locahost at port 6789');
