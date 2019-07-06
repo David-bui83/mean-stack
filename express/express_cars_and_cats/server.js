90d9357d95e7d5cff836a40371ac4c214907bae0
Mona Lisa <david.t.bui83@gmail.com>
2019-07-06 13:41:40 -0700

    cars and cats without ejs

---
+++ express/express_cars_and_cats/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+
+const PORT = 8000;
+
+app.use(express.static(__dirname + '/static'));
+
+
+app.set('views', __dirname + '/views'); 
+app.set('view engine', 'html');
+
+app.get('/',(req,res)=>{
+  res.render('index');
+});
+
+app.get('/cars',(req,res)=>{
+  res.render('cars');
+})
+
+app.get('/cats',(req,res)=>{
+  res.render('cats');
+})
+
+app.get('/form',(req,res)=>{
+  res.render('form');
+})
+
+app.listen(PORT,()=>{console.log(`Listening on ${PORT}`)});
