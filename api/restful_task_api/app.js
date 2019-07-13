326f89a8cb41ca0051906cf7cbf1517ec628743d
Mona Lisa <david.t.bui83@gmail.com>
2019-07-13 13:31:27 -0700

    hello angular

--- api/restful_task_api/app.js
+++ api/restful_task_api/app.js
@@ -17,6 +17,8 @@ app.use(session({
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname,'./static')));
+app.use(express.static(__dirname + '/public/dist/public'));
+// app.use(express.static(path.joing(__dirname,'./public/dist/public')));
 app.use(flash());
 
 app.set('views',path.join(__dirname,'/views'));
