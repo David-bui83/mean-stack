2ede10106902476ca9e6d2954053d36fd5585ea4
Mona Lisa <david.t.bui83@gmail.com>
2019-07-16 10:36:34 -0700

    restful continued

--- angular/dom_manipulation/restful_tasks_continued/public/src/app/app.component.ts
+++ angular/dom_manipulation/restful_tasks_continued/public/src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component,OnInit } from '@angular/core';
 import { HttpService } from './http.service';
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
--- angular/dom_manipulation/restful_tasks_continued/public/src/app/http.service.ts
+++ angular/dom_manipulation/restful_tasks_continued/public/src/app/http.service.ts
@@ -1,17 +1,13 @@
-import { Injectable, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
-export class HttpService implements OnInit{
-
+export class HttpService{
   constructor(private _http: HttpClient) { 
-    // this.getTasks();
   }
   getTasks(){
-    // let tempobservable = this._http.get('/tasks');
-    // tempobservable.subscribe(data=>console.log('Got your tasks!',data));
     return this._http.get('/tasks');
   }
 }
